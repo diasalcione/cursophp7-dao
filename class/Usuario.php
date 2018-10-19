46c1ed88f1ea84394baa2fd73807c4a2c9b1ccab
Alcione Dias <diasalcione@gmail.com>
2018-10-18 22:10:10 -0300

    criamos a classe usuario

--- class/Usuario.php
+++ class/Usuario.php
@@ -1,45 +1,68 @@
 <?php
 
-class Sql extends PDO {
+class Usuario {
 
-	private $conn;
+	private $idusuario;
+	private $deslogin;
+	private $dessenha;
+	private $dtcadastro;
 
-	public function __construct(){
-
-		$this->conn = new PDO("mysql:dbname=dbphp7;host=localhost","root","");
+	public function getIdusuario(){
+		return $this-> $idusuario;
 	}
 
-	private function setParams($statement, $parameters = array()){
-
-		foreach ($parameters as $key => $value){
-
-			$this->setParam($key,$value);
-
-		}
-	} 
-
-	private function setParam($statement, $key, $value){
-
-		$statement->bindParam($key,$value);
+	public function setIdusuario($value){
+		$this->idusuario = $value;
 	}
 
-	public function query($rawQuery,$params = array()){
-
-		$stmt = $this-> conn->prepare($rawQuery);
-
-		$this->setParams($stmt,$params);
-
-		$stmt->execute();
-
-		return $stmt;
+	public function getDeslogin(){
+		return $this-> $deslogin;
 	}
 
-	public function select($rawQuery,$params=array()):array
-	{
-
-		$stmt = $this->query($rawQuery,$params);
-// FETCH_ASSOC traz o nome dos atributos e o valor, não traz (mostra)osíndices
-		return $stmt->fetchAll(PDO::FETCH_ASSOC);
+	public function setDeslogin($value){
+		$this->deslogin = $value;
 	}
+	public function getDessenha(){
+		return $this-> $dessenha;
+	}
+
+	public function setDessenha($value){
+		$this->dessenha = $value;
+	}
+	public function getDtcadastro(){
+		return $this-> $dtcadastro;
+	}
+
+	public function setDtcadastro($value){
+		$this->dtcadastro = $value;
+	}
+
+	public function loadById($id){
+
+        $sql = new Sql();
+
+        $results = $sql->select("SELECT * FROM tb_usuarios WHERE idusuario = :ID", array(":ID"=>$id));
+
+
+        if (count($results) > 0) {
+
+        	$row = $results[0];
+
+        	$this->setIdusuario($row['idusuario']);
+        	$this->setDeslogin($row['deslogin']);
+        	$this->setDessenha($row['dessenha']);
+        	$this->setDtcadastro(new DateTime($row['dtcadastro']));
+        }
+	}
+
+public function __toString(){
+
+	return json_encode(array(
+		"idusuario"=>$this->getIdusuario(),
+		"deslogin"=>$this->getDeslogin(),
+		"dessenha"=>$this->getDessenha(),
+		"dtcadastro"=>$this->getDtcadastro()-> format("d/m/Y H:i:s")));
+}
+
 }
 ?> 
